5a7c1107e4eb1300c3a8420271473e063384f9b4
Andrew Desmarais <terodox@gmail.com>
2013-11-07 11:39:10 -0500

    Initial File Commit

---
+++ js/oldJavascript.js
@@ -0,0 +1,38 @@
+$(function () {
+  
+  $('#getSomeFocus')
+    .bind('blur', function () {
+      console.log("I'm all blurry!");
+    }).bind('click', function () {
+      $('#allEyesOnMe').trigger('focus');
+    });
+  $('#allEyesOnMe').bind('focus', function () {
+    console.log('All eyes are on me!');
+  });
+  //attr vs prop
+  $('#checkItOut').bind('click', function () {
+    var $this = $(this);
+    console.log($this.attr('checked') + ' - ' + $this.prop('checked'));
+  });
+  
+  //Incorrect trigger goodness
+  $('#triggerItWrong').bind('click', function () {
+    var $this = $(this);
+    if($this.attr('checked')) {
+      console.log("I love the feeling of being checked");
+    } else {
+      console.log("I'm not checked");
+    }
+  });
+  $('#triggerClick').bind('click', function () {
+    $('#triggerItWrong').trigger('click');
+  });
+  
+  //live goes bye bye
+  $('#addNewThings').bind('click', function () {
+    $('#allTheThings').append('<div class="oneThing">Yay a thing!' + $('.oneThing').length + '</div>');
+  });
+  $('#allTheThings').delegate('.oneThing', 'click', function () {
+    console.log($(this).text());
+  });
+});
